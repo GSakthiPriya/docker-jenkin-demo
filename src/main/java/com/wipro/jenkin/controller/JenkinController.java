1832db90dea2e527d792d66c5a5c29dd8f695f1b
SP <gsakthimaheswari@gmail.com>
2025-09-24 11:26:36 +0530

    first commit

---
+++ src/main/java/com/wipro/jenkin/controller/JenkinController.java
@@ -0,0 +1,13 @@
+package com.wipro.jenkin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequestMapping("/api/v1/jenkin")
+@RestController
+public class JenkinController {
+	@GetMapping
+	public String welcome() {
+		return "Welcome to Jenkin Pipeline";
+	}
+}
